541a7a3f18b95f39f66db3a02e8929cce7335459
chengzh2008 <chengzh2008@gmail.com>
2022-11-11 12:16:51 -0800

    finish cat show

--- src/main/scala/sandbox/CatShow.scala
+++ src/main/scala/sandbox/CatShow.scala
@@ -1,21 +1,26 @@
 package sandbox
 
 import cats.Show
-import cats.instances.int._    // for Show
+import cats.instances.int._ // for Show
 import cats.instances.string._ // for Show
-import cats.syntax.show._      // for show
+import cats.syntax.show._ // for show
 
-final case class Cat(name: String, age: Int, color: String)
-
-implicit val catShow: Show[Cat] = Show.show[Cat] { cat =>
-  val name  = cat.name.show
-  val age   = cat.age.show
-  val color = cat.color.show
-  s"$name is a $age year-old $color cat."
+package object catShow {
+  implicit val catShow: Show[Cat] = Show.show[Cat] { cat =>
+    val name = cat.name.show
+    val age = cat.age.show
+    val color = cat.color.show
+    s"$name is a $age year-old $color cat."
+  }
 }
 
-object CatShow extends App {
+import catShow._
+
+object CatShowTest extends App {
   val showInt = Show.apply[Int]
-  123.show
-  showInt.show(123)
+  println(123.show)
+  println(showInt.show(123))
+
+  val cat = Cat("Garfield", 41, "ginger and black")
+  println(catShow.show(cat))
 }
