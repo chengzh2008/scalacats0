fc871f8a2778a4334fefbf6e9cf1d95ee68f6838
chengzh2008 <chengzh2008@gmail.com>
2022-11-15 21:04:40 -0800

    update monaderror

--- src/main/scala/sandbox/cats4/Abstracting.scala
+++ src/main/scala/sandbox/cats4/Abstracting.scala
@@ -8,12 +8,17 @@ object Abstracting {
   def validateAdult[F[_]](age: Int)(implicit
       me: MonadError[F, Throwable]
   ): F[Int] =
-    me.ensure(me.pure(age))(new IllegalArgumentException())(_ >= 18)
+    me.ensure(me.pure(age))(
+      new IllegalArgumentException("Age must be greater than or equal to 18")
+    )(_ >= 18)
 }
 
 object AbstractingTest extends App {
   // Success(18)
   println(Abstracting.validateAdult[Try](18))
-  // Failure(java.lang.IllegalArgumentException)
-  println(Abstracting.validateAdult[Try](8))
+  // Failure(java.lang.IllegalArgumentException: Age must be greater than or equal to 18)
+
+  type ExceptionOr[A] = Either[Throwable, A]
+  // Left(java.lang.IllegalArgumentException: Age must be greater than or equal to 18)
+  println(Abstracting.validateAdult[ExceptionOr](-1))
 }
