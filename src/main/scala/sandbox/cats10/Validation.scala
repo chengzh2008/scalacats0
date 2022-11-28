d291e659b40a286cf45a6f99b590184233762d5c
chengzh2008 <chengzh2008@gmail.com>
2022-11-27 19:37:32 -0800

    update either to be validated

--- src/main/scala/sandbox/cats10/Validation.scala
+++ src/main/scala/sandbox/cats10/Validation.scala
@@ -1,21 +1,18 @@
 package sandbox.cats10
 
 import cats.Semigroup
-import cats.syntax.either._
 import cats.syntax.semigroup._
+import cats.data.Validated
+import cats.data.Validated._
+import cats.syntax.apply._
 
 // implementation 1) functional wrapper with a class
-final case class CheckF[E: Semigroup, A](f: A => Either[E, A]) {
-  def apply(a: A): Either[E, A] = f(a)
+final case class CheckF[E: Semigroup, A](f: A => Validated[E, A]) {
+  def apply(a: A): Validated[E, A] = f(a)
 
   def and(that: CheckF[E, A]): CheckF[E, A] =
     CheckF { a =>
-      (this(a), that(a)) match {
-        case (Left(v1), Left(v2)) => (v1 |+| v2).asLeft
-        case (Left(v1), Right(_)) => v1.asLeft
-        case (Right(_), Left(v2)) => v2.asLeft
-        case (Right(_), Right(_)) => a.asRight
-      }
+      (this(a), that(a)).mapN((_, _) => a)
     }
 }
 
@@ -27,20 +24,20 @@ sealed trait Check[E, A] {
 
   def or(that: Check[E, A]): Check[E, A] = Or(this, that)
 
-  def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] =
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
     this match {
       case Pure(f) => f(a)
       case And(l, r) =>
-        (l(a), r(a)) match {
-          case (Left(v1), Left(v2)) => (v1 |+| v2).asLeft
-          case (Left(v1), Right(_)) => v1.asLeft
-          case (Right(_), Left(v2)) => v2.asLeft
-          case (Right(_), Right(_)) => a.asRight
-        }
+        (l(a), r(a)).mapN((_, _) => a)
       case Or(l, r) =>
-        (l(a), r(a)) match {
-          case (Left(v1), Left(v2)) => (v1 |+| v2).asLeft
-          case (_, _)               => a.asRight
+        l(a) match {
+          case Valid(a) => Valid(a)
+          case Invalid(e1) => {
+            r(a) match {
+              case Valid(a)    => Valid(a)
+              case Invalid(e2) => Invalid(e1.combine(e2))
+            }
+          }
         }
     }
 }
@@ -51,9 +48,9 @@ object Check {
   final case class Or[E, A](left: Check[E, A], right: Check[E, A])
       extends Check[E, A]
 
-  final case class Pure[E, A](f: A => Either[E, A]) extends Check[E, A]
+  final case class Pure[E, A](f: A => Validated[E, A]) extends Check[E, A]
 
-  def pure[E, A](f: A => Either[E, A]): Check[E, A] = Pure(f)
+  def pure[E, A](f: A => Validated[E, A]): Check[E, A] = Pure(f)
 }
 
 object ValidationTest extends App {
@@ -61,39 +58,39 @@ object ValidationTest extends App {
 
   // testing for implementation 1
   val a: CheckF[List[String], Int] = CheckF { v =>
-    if (v > 2) v.asRight
-    else List("Must be > 2").asLeft
+    if (v > 2) Valid(v)
+    else Invalid(List("Must be > 2"))
   }
 
   val b: CheckF[List[String], Int] = CheckF { v =>
-    if (v < -2) v.asRight
-    else List("Must be < -2").asLeft
+    if (v < -2) Valid(v)
+    else Invalid(List("Must be < -2"))
   }
 
   val check: CheckF[List[String], Int] = a and b
 
-  // Left(List(Must be < -2))
+  // Invalid(List(Must be < -2))
   println(check(5))
-  // Left(List(Must be > 2, Must be < -2))
+  // Invalid(List(Must be > 2, Must be < -2))
   println(check(0))
 
   // testing for implementation 2
   val c: Check[List[String], Int] = Check.pure { v =>
-    if (v > 2) v.asRight
-    else List("Must be > 2").asLeft
+    if (v > 2) Valid(v)
+    else Invalid(List("Must be > 2"))
   }
   val d: Check[List[String], Int] = Check.pure { v =>
-    if (v < -2) v.asRight
-    else List("Must be < -2").asLeft
+    if (v < -2) Valid(v)
+    else Invalid(List("Must be < -2"))
   }
 
   val check2 = c.and(d)
   val check3 = c.or(d)
-  // Left(List(Must be < -2))
+  // Invalid(List(Must be < -2))
   println(check2.apply(5))
-  // Left(List(Must be > 2, Must be < -2))
+  // Invalid(List(Must be > 2, Must be < -2))
   println(check2.apply(0))
 
-  // Right(5)
+  // Valid(5)
   println(check3.apply(5))
 }
