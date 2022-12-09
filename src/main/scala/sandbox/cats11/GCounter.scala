683e440fac666b7e200e18427beb40bc2d32d7e3
chengzh2008 <chengzh2008@gmail.com>
2022-12-09 08:42:24 -0800

    implement GCounter trait for map

--- src/main/scala/sandbox/cats11/GCounter.scala
+++ src/main/scala/sandbox/cats11/GCounter.scala
@@ -1,7 +1,9 @@
 package sandbox.cats11
 
-import cats.syntax.monoid._
 import cats.kernel.CommutativeMonoid
+import cats.instances.map._
+import cats.syntax.semigroup._
+import cats.syntax.foldable._
 
 trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
   def combine(x: A, y: A): A
@@ -43,4 +45,60 @@ final case class GCounter[A](counters: Map[String, A]) {
     )
   def total(implicit m: CommutativeMonoid[A]): A =
     counters.values.foldLeft(m.empty)(m.combine)
+}
+
+trait GGCounter[F[_, _], K, V] {
+  def increment(f: F[K, V])(k: K, v: V)(implicit
+      m: CommutativeMonoid[V]
+  ): F[K, V]
+
+  def merge(f1: F[K, V], f2: F[K, V])(implicit
+      b: BoundedSemiLattice[V]
+  ): F[K, V]
+
+  def total(f: F[K, V])(implicit m: CommutativeMonoid[V]): V
+}
+
+object GGCounter {
+  def apply[F[_, _], K, V](implicit
+      counter: GGCounter[F, K, V]
+  ): GGCounter[F, K, V] = counter
+}
+
+object GGCounterInstance {
+  implicit def mapGGCounterInsntace[K, V]: GGCounter[Map, K, V] =
+    new GGCounter[Map, K, V] {
+      def increment(
+          f: Map[K, V]
+      )(k: K, v: V)(implicit m: CommutativeMonoid[V]): Map[K, V] = {
+        val nv = v |+| f.getOrElse(k, m.empty)
+        f + (k -> nv)
+      }
+
+      def merge(f1: Map[K, V], f2: Map[K, V])(implicit
+          b: BoundedSemiLattice[V]
+      ): Map[K, V] = {
+        f1 |+| f2
+      }
+
+      def total(f: Map[K, V])(implicit m: CommutativeMonoid[V]): V =
+        f.values.toList.combineAll
+    }
+
+}
+
+object GCounter extends App {
+  import GGCounterInstance._
+  import BoundedSemiLatticeInstances._
+
+  val g1 = Map("a" -> 7, "b" -> 3)
+  val g2 = Map("a" -> 2, "b" -> 5)
+  //
+  val counter = GGCounter[Map, String, Int]
+  val merged = counter.merge(g1, g2)
+  println(merged)
+  println(merged.values)
+  val total = counter.total(merged)
+  // 10 TODO which is not correct, why?
+  println(total)
 }
